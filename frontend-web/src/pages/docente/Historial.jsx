a19750e0d911453caaf499a2eaa988a525f44bac
Eduard Mejia Cruz <eduardmejiacruz6@micorreo.upp.edu.mx>
2025-11-05 00:11:44 -0600

    Creacion de vista Historial Docente

--- frontend-web/src/pages/docente/Historial.jsx
+++ frontend-web/src/pages/docente/Historial.jsx
@@ -1,8 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import "./docente.css";
+
 export default function Historial() {
+  const nav = useNavigate();
+
   return (
     <div className="page-shell">
-      <h2>Escanear QR</h2>
-      {/* contenido */}
+      <div className="menu-card">
+        <button className="back-btn" onClick={() => nav(-1)}>←</button>
+
+        <h2 className="center-title">Historial</h2>
+
+        <div className="form">
+          <input type="text" placeholder="Laboratorio" />
+          <input type="text" placeholder="Rango de Fechas" />
+
+          <button className="btn-primary small">Generar PDF</button>
+        </div>
+
+        <div className="class-list">
+          <div className="class-item success">
+            <p><b>A-93 Programación visual</b><br />viernes 10:00 - 12:00</p>
+            <span>Registrada</span>
+          </div>
+
+          <div className="class-item success">
+            <p><b>L-Antenas Tutoría</b><br />viernes 7:00 - 8:00</p>
+            <span>Registrada</span>
+          </div>
+
+          <div className="class-item danger">
+            <p><b>A-93 Estancia 1</b><br />viernes 9:00 - 10:00</p>
+            <span>Fuera de horario</span>
+          </div>
+
+          <div className="class-item warning">
+            <p><b>A-93 Estancia 1</b><br />viernes 9:00 - 10:00</p>
+            <span>No asistió</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
