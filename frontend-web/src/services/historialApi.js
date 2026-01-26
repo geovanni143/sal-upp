a23e61a0c521932c7552c40290eb0f5fb0e029cc
Eduard Mejia Cruz <eduardmejiacruz6@micorreo.upp.edu.mx>
2026-01-26 00:33:48 -0600

    arreglo de url de api de historial administrador

--- frontend-web/src/services/historialApi.js
+++ frontend-web/src/services/historialApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:3000/api/historial";
+const API = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
 
 // Filtros iniciales (periodos + labs)
 export const getFiltrosHistorial = async () => {
